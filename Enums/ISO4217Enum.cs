4046d12e4c4d59fe522a3852f49a0f6be9f0e09f
Iryna <ortofosfatnyi@gmail.com>
2021-01-18 13:11:18 +0200

    Making Enum of currencies

---
+++ Enums/ISO4217Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modyl_03_try.Enums
+{
+    public enum ISO4217Enum // international standard of currency codes
+    {
+        UAH = 980,
+        USD = 840,
+        EUR = 978,
+    }
+}
